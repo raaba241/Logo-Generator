0d299badaf981de1ae13d3668828f2a5749fcd64
unknown <raaba21@gmail.com>
2024-03-16 17:59:21 -0400

    added a processing from the inquirer promp and creates an SVG file from given inputs

--- index.js
+++ index.js
@@ -27,3 +27,24 @@ const questions = [
   }
 ];
 
+inquirer.prompt(questions).then(answers => {
+    let shapeSVG;
+  
+    if (answers.shape === 'Circle') {
+      shapeSVG = new Circle(answers.shapeColor);
+    } else if (answers.shape === 'Triangle') {
+      shapeSVG = new Triangle(answers.shapeColor);
+    } else if (answers.shape === 'Square') {
+      shapeSVG = new Square(answers.shapeColor);
+    }
+  
+    const svgContent = `
+  <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+    ${shapeSVG.render()}
+    <text x="150" y="110" font-family="Arial" font-size="20" fill="${answers.textColor}" text-anchor="middle">${answers.text}</text>
+  </svg>
+    `;
+  
+    fs.writeFileSync('logo.svg', svgContent);
+    console.log('Generated logo.svg');
+  });
