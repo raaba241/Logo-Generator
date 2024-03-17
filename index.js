d273068b432d269c4ddd2aa6402b6095edd54582
unknown <raaba21@gmail.com>
2024-03-17 16:23:28 -0400

    added comments to the project

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const { Circle, Triangle, Square } = require('./lib/shapes');
 
+// questions for the user
 const questions = [
   {
     type: 'input',
@@ -27,24 +28,31 @@ const questions = [
   }
 ];
 
+// prompt the user for input
 inquirer.prompt(questions).then(answers => {
+    // creates the shape based on the user's input
     let shapeSVG;
-  
+    
+
     if (answers.shape === 'Circle') {
       shapeSVG = new Circle(answers.shapeColor);
+
     } else if (answers.shape === 'Triangle') {
       shapeSVG = new Triangle(answers.shapeColor);
+
     } else if (answers.shape === 'Square') {
       shapeSVG = new Square(answers.shapeColor);
     }
-  
-    const svgContent = `
+    
+    // creates the SVG content
+    const svgLogo = `
   <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
     ${shapeSVG.render()}
     <text x="150" y="110" font-family="Arial" font-size="20" fill="${answers.textColor}" text-anchor="middle">${answers.text}</text>
   </svg>
     `;
   
-    fs.writeFileSync('logo.svg', svgContent);
-    console.log('Generated logo.svg');
+    // writes the SVG content to the file
+    fs.writeFileSync('logo.svg', svgLogo);
+    console.log('logo.svg has been created!');
   });
